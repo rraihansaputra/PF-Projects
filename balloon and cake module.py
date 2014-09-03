6de40e17b67c97d93364286aa92a31d4ed916bf3
rraihansaputra <R@Raihans-MacBook-Air.local>
2014-09-03 14:24:53 +0700

    functioning version
    after a few tries, working as intended

---
+++ balloon and cake module.py
@@ -0,0 +1,16 @@
+
+def balloon(name):
+    print ("I am", name, "and I love Baloons")
+    IfFunc(name)
+def cake(name):
+    print ("I am", name, "and I hate cakes")
+    IfFunc(name)
+def IfFunc(name):
+    if name == 'eros':
+        print ('yeh', name)
+    else:
+        print ('lafyu')
+
+print ('cake and baloons module is ready')
+print ('by the way')
+print ('i hate you')
